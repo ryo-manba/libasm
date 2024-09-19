5d128c13eb85ed649c4c4828c72d77851d1cf078
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-09-19 17:15:47 +0900

    add read.s

--- ft_read.s
+++ ft_read.s
@@ -1,17 +1,17 @@
-; ssize_t write(int fildes, const void *buf, size_t nbyte)
-	global _ft_write
+; ssize_t read(int fildes, void *buf, size_t nbyte)
+  global _ft_read
   extern ___error
-	section .text
+  section .text
 
 ; rdi = fildes, rsi = buf, rdx = nbyte
-_ft_write:
-  mov rax, 0x2000004 ; write syscall number
-	syscall
-	jc .error
-	ret
+_ft_read:
+  mov rax, 0x2000003 ; read syscall number
+  syscall
+  jc .error
+  ret
 
 .error:
-	mov r8, rax
+  mov r8, rax
 	call ___error ; Call ___error to set the address of errno in rax
 	mov [rax], r8 ;  Restore the error code in rax
 	mov rax, -1
--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 size_t ft_strlen(const char *s);
 char *ft_strcpy(char *dst, const char *src);
 int ft_strcmp(const char* s1, const char *s2);
 ssize_t ft_write(int fildes, const void *buf, size_t nbyte);
+ssize_t ft_read(int fildes, const void *buf, size_t nbyte);
 
 bool test_strlen(void) {
   const char *s = "hello world";
@@ -52,7 +54,7 @@ bool test_strcmp(void) {
   return std_res1 == ft_res1 && std_res2 == ft_res2 && std_res3 == ft_res3;
 }
 
-bool test_write(void) {
+void test_write(void) {
   const char *s1 = "hello";
   const char *s2 = "world";
 
@@ -75,11 +77,44 @@ bool test_write(void) {
 
   printf("std_res1: %zd, std_res2: %zd, std_res3: %zd, std_res4: %zd\n", std_res1, std_res2, std_res3, std_res4);
   printf("ft_res1: %zd, ft_res2: %zd, ft_res3: %zd, ft_res4: %zd\n", ft_res1, ft_res2, ft_res3, ft_res4);
+}
 
-  return true;
+void test_read(void) {
+  char std_buf[1024];
+  char ft_buf[1024];
+  memset(std_buf, '\0', sizeof(std_buf));
+  // memset(ft_buf, '\0', sizeof(ft_buf));
+
+  const int fd1 = open("./testfiles/hello1.txt", O_RDONLY);
+  const int fd2 = open("./testfiles/hello2.txt", O_RDONLY);
+  if (fd1 < 0 || fd2 < 0) {
+    printf("failed to open");
+    return;
+  }
+  int std_res;
+  int ft_res;
+  int i = 0;
+  while (i++ < 3) {
+    std_res = read(fd1, std_buf, 10);
+    ft_res = ft_read(fd2, ft_buf, 10);
+    printf("std_res: %d, buf: %s\n", std_res, std_buf);
+    printf("ft_res : %d, buf: %s\n", ft_res, ft_buf);
+  }
+
+  // error case
+  printf("1.errno: %d\n", errno);
+  std_res = read(42, std_buf, 10);
+  printf("2.errno: %d\n", errno);
+  errno = 0;
+  printf("3.errno: %d\n", errno);
+  ft_res = ft_read(42, ft_buf, 10);
+  printf("4.errno: %d\n", errno);
+
+  printf("std_res: %d, buf: %s\n", std_res, std_buf);
+  printf("ft_res : %d, buf: %s\n", ft_res, ft_buf);
 }
 
 
 int main(void) {
-  test_write();
+  test_read();
 }
