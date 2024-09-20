bd27c85db32d30909554ce89a5169bf1a331ae1e
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-09-20 14:49:07 +0900

    fix using zero extension to support multibyte strings

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -5,30 +5,25 @@
 ; s1 = rdi, s2 = rsi
 _ft_strcmp:
   xor rcx, rcx
-  push rbx
 
 .loop:
   mov al, BYTE[rdi + rcx]
+  mov r8b, BYTE[rsi + rcx]
+  cmp al, r8b
+  jne .diff
+
   cmp al, 0
   je .done
-
-  mov bl, BYTE[rsi + rcx]
-  cmp bl, 0
-  je .done
-
-  cmp al, bl
-  jne .diff
 
   inc rcx
   jmp .loop
 
 .diff:
-  sub al, bl
-  movsx rax, al ; see: https://www.sigbus.info/compilerbook#%E3%82%B9%E3%83%86%E3%83%83%E3%83%9724-%E6%96%87%E5%AD%97%E5%9E%8B%E3%82%92%E5%AE%9F%E8%A3%85%E3%81%99%E3%82%8B
-  pop rbx
+  movzx rax, al ; zero extension to support multibyte strings
+  movzx r8, r8b
+  sub rax, r8
   ret
 
 .done:
   xor rax, rax
-  pop rbx
   ret
